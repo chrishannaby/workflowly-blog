8b29261f284bbea8688c6dc7a9e24bfb56f93d50
Chris Hannaby <chris.hannaby@netlify.com>
2021-07-27 10:51:54 -0600

    don't use basepath

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 
 module.exports = {
   target: "serverless",
-  basePath: "/blog",
   webpack(config) {
     config.resolve.modules.push(path.resolve("./"));
     return config;
